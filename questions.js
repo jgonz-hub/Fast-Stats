563725c60f9276b950ccc7d0c162bbc3f53e57ea
Jon Gonzalez <73318077+jgonz-hub@users.noreply.github.com>
2022-08-03 13:52:43 -0500

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,118 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+    numb: 1,
+    question: "The mean is:",
+    answer: "The average of the numbers in a dataset",
+    options: [
+      "A nickname for your unfriendly friend",
+      "The average of the numbers in a dataset",
+      "The middle number in a dataset",
+      "The most common number in a dataset"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which of the following datasets has a variance of 0?",
+    answer: "7, 7, 7, 7, 7",
+    options: [
+      "1, 2, 13, 2, 5",
+      "5, 8, 0, 8, 2",
+      "1, 2, 3, 4, 5",
+      "7, 7, 7, 7, 7"
+      
+    ]
+  },
+    {
+    numb: 3,
+    question: "A parameter is:",
+    answer: "A Population Characteristic",
+    options: [
+      "A Sample Characteristic",
+      "A Population Characteristic",
+      "Unknown",
+      "Normal Normally Distributed"
+
+    
+    ]
+  },
+    {
+    numb: 4,
+    question: "What is an advantage of using volunteers in a study?",
+    answer: "Volunteers are more willing to take part in the study ",
+    options: [
+      "Volunteer sampling guarantees a representative sample of the population",
+      "Some volunteers do not complete the study",
+      "Volunteers are more willing to take part in the study ",
+      "Volunteer sampling prevents under coverage bias"
+      
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which one of the following variables is NOT categorical?",
+    answer: "Height of a Person",
+    options: [
+      "Age of a Person",
+      "Gender of a Person: Male or Female",
+      "Choice on a Test Item: True or False",
+      "Height of a Person"
+
+    ]
+  },
+    {
+    numb: 6,
+    question: "A magazine printed a survey in its monthly issue and asked readers to fill it out and send it in. Over 1000 readers did so. This type of sample is called",
+    answer: "A Self-Selected Sample",
+    options: [
+      "A Cluster Sample",
+      "A Self-Selected Sample",
+      "A Stratified Sample",
+      "A Simple Random Sample"
+    ]
+  },
+    {
+    numb: 7,
+    question: "Which one of these statistics is unaffected by outliers?",
+    answer: "Interquartile Range",
+    options: [
+      "Mean",
+      "Interquartile Range",
+      "Standard Deviation",
+      "Range"
+    ]
+  },
+    {
+    numb: 8,
+    question: "What is a p-value?",
+    answer: "None of the above",
+    options: [
+      "The probability of rejecting the null hypothesis ",
+      "The probability of failing to reject the null hypothesis ",
+      "The probability of a type one error",
+      "None of the above"
+    ]
+  },
+  {
+    numb: 9,
+    question: "A sampling technique is used in Qualitative research wherein the researcher chooses individuals who are easily accessible to become respondents for the study.",
+    answer: "Convenience Sampling",
+    options: [
+      "Judgement Sampling",
+      "Convenience Sampling",
+      "Snowball Sampling",
+      "Quota Sampling"
+    ]
+  },
+    {
+    numb: 10,
+    question: "Failing to reject the null hypothesis when it is false is:",
+    answer: "Type II Error",
+    options: [
+      "Alpha",
+      "Type I Error",
+      "Type II Error",
+      "Beta"
+    ]
+},
+];
